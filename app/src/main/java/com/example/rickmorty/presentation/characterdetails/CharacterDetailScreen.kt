ec332a642c467913fcf6ed9776912b2e8987676b
Alex Gallardo <alexgallardo11@gmail.com>
2025-05-29 16:26:23 +0200

    Refactor: Add background image to CharacterDetailScreen

--- app/src/main/java/com/example/rickmorty/presentation/characterdetails/CharacterDetailScreen.kt
+++ app/src/main/java/com/example/rickmorty/presentation/characterdetails/CharacterDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.example.rickmorty.presentation.characterdetails
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,9 +23,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.rickmorty.R
 import com.example.rickmorty.presentation.characterdetails.components.CharacterEpisodesInfo
 import com.example.rickmorty.presentation.characterdetails.components.CharacterImage
 import com.example.rickmorty.presentation.characterdetails.components.CharacterLocationInfo
@@ -51,6 +56,12 @@ fun CharacterDetailScreen(
       .fillMaxSize(),
 
     ) {
+    Image(
+      painter = painterResource(id = R.drawable.background),
+      contentDescription = "background",
+      modifier = Modifier.fillMaxSize().alpha(0.5f),
+      contentScale = ContentScale.Crop
+    )
     when (uiState) {
       is CharacterDetailUiState.Loading -> {
         Box(
